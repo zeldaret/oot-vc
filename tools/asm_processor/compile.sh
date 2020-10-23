acfb97b06e698cf7605d70273820d913c91e9a39
Thar0 <17233964+Thar0@users.noreply.github.com>
2020-10-23 21:01:18 +0100

    Allow building with wsl1

--- tools/asm_processor/compile.sh
+++ tools/asm_processor/compile.sh
@@ -5,10 +5,13 @@ shift
 AS="$1"
 shift
 
-temp="$(mktemp)"
+# When running windows exes through wsl1 you can't give them linux paths
+mkdir tools/asm_processor/tmp
+temp="$(mktemp --tmpdir=tools/asm_processor/tmp)"
 
 tools/asm_processor/asm_processor.py "$2" --assembler "$AS" > "$temp.c" &&
 $CC "$temp.c" -c -o "$temp.o" &&
-tools/asm_processor/asm_processor.py "$2" --post-process "$temp.o" --assembler "$AS" --asm-prelude include/macros.inc &&
+tools/asm_processor/asm_processor.py "$2" --post-process "$temp.o" --assembler "$AS" --asm-prelude include/macros.inc
 powerpc-eabi-objcopy --remove-section .mwcats.text "$temp.o" "$temp.2.o"
 powerpc-eabi-objcopy --remove-section .comment "$temp.2.o" "$1"
+rm -rf tools/asm_processor/tmp
