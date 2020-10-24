49781435c118b7f5b218f7b9a7dff79359087d3a
KrimtonZ <krimtonz@gmail.com>
2020-10-24 07:56:09 -0500

    split xlObject

---
+++ src/virtual_console/xlObject.c
@@ -0,0 +1,11 @@
+GLOBAL_ASM("asm/non_matchings/virtual_console/xlObject/xlObjectMake.s")
+
+GLOBAL_ASM("asm/non_matchings/virtual_console/xlObject/xlObjectFree.s")
+
+GLOBAL_ASM("asm/non_matchings/virtual_console/xlObject/func_800823EC.s")
+
+GLOBAL_ASM("asm/non_matchings/virtual_console/xlObject/xlObjectEvent.s")
+
+GLOBAL_ASM("asm/non_matchings/virtual_console/xlObject/func_80082514.s")
+
+GLOBAL_ASM("asm/non_matchings/virtual_console/xlObject/xlObjectReset.s")
