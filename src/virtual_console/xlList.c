4b5d700dc4cf931977608c36fe0fef02954083cd
KrimtonZ <krimtonz@gmail.com>
2020-10-24 21:16:13 -0500

    split xlList

---
+++ src/virtual_console/xlList.c
@@ -0,0 +1,13 @@
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/xlList/func_800805F0.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/xlList/rspFreeList.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/xlList/func_800807A0.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/xlList/xlListFreeItem.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/xlList/xlListTestItem.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/xlList/func_80080980.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/xlList/func_800809A8.s")
