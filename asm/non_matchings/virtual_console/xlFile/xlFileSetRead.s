786dfe41a6ad75cb9ceaa9978dc50b2c068d0dc3
Thar0 <17233964+Thar0@users.noreply.github.com>
2020-11-30 05:48:21 +0000

    Split xlFile

---
+++ asm/non_matchings/virtual_console/xlFile/xlFileSetRead.s
@@ -0,0 +1,3 @@
+glabel xlFileSetRead
+/* 800800CC 0007B68C  38 60 00 01 */	li r3, 1
+/* 800800D0 0007B690  4E 80 00 20 */	blr 
---
+++ src/virtual_console/xlFile.c
@@ -0,0 +1,20 @@
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/xlFile/xlFileSetOpen.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/xlFile/xlFileSetRead.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/xlFile/func_800800D4.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/xlFile/func_800801BC.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/xlFile/func_800801E8.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/xlFile/func_8008039C.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/xlFile/func_800803C4.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/xlFile/func_80080534.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/xlFile/func_80080558.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/xlFile/func_80080590.s")
