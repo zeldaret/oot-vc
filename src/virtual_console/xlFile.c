352d6e04cd4922808ba492d18ad52bff1d551b37
Thar0 <17233964+Thar0@users.noreply.github.com>
2020-12-09 14:57:08 +0000

    Split xlText from xlFile

--- src/virtual_console/xlFile.c
+++ src/virtual_console/xlFile.c
@@ -170,38 +170,3 @@ s32 fileEvent(file_t *file, event_t event, void *arg) {
     }
     return 1;
 }
-
-s32 func_80080534(char *str) {
-    s32 iVar1 = 0;
-    while (str[iVar1] != '\x00') {
-        iVar1++;
-    }
-    return iVar1;
-}
-
-s32 func_80080558(char *str1, char *str2) {
-    s32 iVar2 = 0;
-
-    while (str2[iVar2] != '\x00') {
-        str1[iVar2] = str2[iVar2];
-        iVar2++;
-    }
-    str1[iVar2] = '\x00';
-    return iVar2;
-}
-
-s32 func_80080590(char *str1, char *str2) {
-    s32 iVar3;
-    s32 iVar4;
-
-    iVar4 = 0;
-    while(str1[iVar4] != '\x00') {
-        iVar4++;
-    }
-    iVar3 = 0;
-    while(str2[iVar3] != '\x00') {
-        str1[iVar4++] = str2[iVar3++];
-    }
-    str1[iVar4] = '\x00';
-    return iVar3;
-}
