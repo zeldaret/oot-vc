2b0cf99313f9b7a75b315a28c80c1c0c259a739d
Thar0 <maximilianc64@gmail.com>
2020-12-19 02:33:06 +0000

    GLOBAL_ASM rom.c

---
+++ src/virtual_console/rom.c
@@ -0,0 +1,51 @@
+#include "types.h"
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/rom/func_800431d4.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/rom/func_800433ec.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/rom/func_8004341c.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/rom/func_80043424.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/rom/func_8004342c.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/rom/func_80043434.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/rom/func_8004343c.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/rom/func_800434a4.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/rom/func_8004350c.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/rom/func_80043574.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/rom/func_800435e4.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/rom/func_8004369c.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/rom/func_800436a4.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/rom/func_800436ac.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/rom/func_800436b4.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/rom/func_800436bc.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/rom/func_800436cc.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/rom/func_800436dc.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/rom/func_800436ec.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/rom/func_80043700.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/rom/romUpdate.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/rom/romSetImage.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/rom/func_80043dc4.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/rom/func_80043e0c.s")
+
+#pragma GLOBAL_ASM("asm/non_matchings/virtual_console/rom/func_80043e70.s")
