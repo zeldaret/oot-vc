0160c101a3346750aaec0a8f980228a5e3f7c0dc
Yanis42 <35189056+Yanis42@users.noreply.github.com>
2024-08-28 22:31:30 +0200

    removed the older libc files

--- include/math.h
+++
@@ -1,8 +0,0 @@
-#ifndef _MATH_H
-#define _MATH_H
-
-double sqrt(double in);
-double ceil(double in);
-double floor(double in);
-
-#endif
--- include/stddef.h
+++
@@ -1,6 +0,0 @@
-#ifndef _STDDEF_H
-#define _STDDEF_H
-
-#define offsetof(s,m) ((size_t)&(((s*)0)->m))
-
-#endif
