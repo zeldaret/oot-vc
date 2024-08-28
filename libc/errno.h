47dd62e3b952f2caa3ecde4c029f6d43857d92ca
Yanis42 <35189056+Yanis42@users.noreply.github.com>
2024-08-28 22:00:15 +0200

    add libc headers

---
+++ libc/errno.h
@@ -0,0 +1,13 @@
+#ifndef _ERRNO_H_
+#define _ERRNO_H_
+
+extern int errno;
+
+#define ENOERR 0
+#define EDOM 33
+#define ERANGE 34
+#define ESIGPARM 36
+#define EFPOS 40
+#define EILSEQ 84
+
+#endif
