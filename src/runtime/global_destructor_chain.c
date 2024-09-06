783f23e25329db02b833d9bdb4c270676308aafb
Yanis <35189056+Yanis42@users.noreply.github.com>
2024-09-07 00:08:15 +0200

    Add libc splits and complete runtime ones (#46)
    * libc splits
    
    * fixed s_cos filename in the configuration
    
    * name mystery functions (match?)
    
    * forgot to rename files in the configuration again
    
    * fixed build issue
    
    * add missing libc files and complete runtime splits
    
    * review

--- src/runtime/global_destructor_chain.c
+++ src/runtime/global_destructor_chain.c
@@ -4,6 +4,15 @@
 
 DestructorChain* __global_destructor_chain;
 
+void* __register_global_object(void* object, void* destructor, void* regmem) {
+    ((DestructorChain*)regmem)->next = __global_destructor_chain;
+    ((DestructorChain*)regmem)->destructor = destructor;
+    ((DestructorChain*)regmem)->object = object;
+    __global_destructor_chain = (DestructorChain*)regmem;
+
+    return object;
+}
+
 void __destroy_global_chain(void) {
     DestructorChain* iter;
 
