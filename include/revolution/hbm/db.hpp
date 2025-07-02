4b0c529bc237cf7b85f01437c107fa5e024044f8
Yanis002 <35189056+Yanis002@users.noreply.github.com>
2025-07-03 01:23:55 +0200

    remove useless files

--- include/revolution/hbm/db.hpp
+++ include/revolution/hbm/db.hpp
@@ -1,11 +1,9 @@
 #ifndef _REVOLUTION_HBM_NW4HBM_DB_HPP
 #define _REVOLUTION_HBM_NW4HBM_DB_HPP
 
-#include "revolution/hbm/nw4hbm/db/DbgPrintBase.hpp"
 #include "revolution/hbm/nw4hbm/db/assert.hpp"
 #include "revolution/hbm/nw4hbm/db/console.hpp"
 #include "revolution/hbm/nw4hbm/db/directPrint.hpp"
-#include "revolution/hbm/nw4hbm/db/exception.hpp"
 #include "revolution/hbm/nw4hbm/db/mapFile.hpp"
 
 #endif
