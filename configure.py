05cd58ad58ac1cb59d1fb6aac9979bb5e5c6821d
Yanis002 <35189056+Yanis002@users.noreply.github.com>
2025-09-17 23:46:14 +0200

    match lyt_group

--- configure.py
+++ configure.py
@@ -653,7 +653,7 @@ config.libs = [
             Object(LinkedFor("oot-j"), "revolution/hbm/nw4hbm/lyt/lyt_bounding.cpp"),
             Object(LinkedFor("oot-j"), "revolution/hbm/nw4hbm/lyt/lyt_common.cpp"),
             Object(LinkedFor("oot-j"), "revolution/hbm/nw4hbm/lyt/lyt_drawInfo.cpp"),
-            Object(NotLinked, "revolution/hbm/nw4hbm/lyt/lyt_group.cpp"),
+            Object(LinkedFor("oot-j"), "revolution/hbm/nw4hbm/lyt/lyt_group.cpp"),
             Object(NotLinked, "revolution/hbm/nw4hbm/lyt/lyt_layout.cpp"),
             Object(NotLinked, "revolution/hbm/nw4hbm/lyt/lyt_material.cpp"),
             Object(NotLinked, "revolution/hbm/nw4hbm/lyt/lyt_pane.cpp"),
