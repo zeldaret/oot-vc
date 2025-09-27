2da23233ab9ee511c0a9071596795e10bd13d5e8
cadmic <cadmic24@gmail.com>
2025-09-27 11:12:11 -0700

    Runtime progress (#92)
    * progress
    
    * Work on operator new()
    
    * Remove throw() from malloc/free declarations
    
    ---------
    
    Co-authored-by: Yanis002 <35189056+Yanis002@users.noreply.github.com>

---
+++ include/new.hpp
@@ -0,0 +1,6 @@
+#ifndef __STDCXX_NEW__
+#define __STDCXX_NEW__
+
+#include "std_size_t.hpp"
+
+#endif
---
+++ include/std_size_t.hpp
@@ -0,0 +1,8 @@
+#ifndef __INTERNAL_STD_SIZE_T_HPP__
+#define __INTERNAL_STD_SIZE_T_HPP__
+
+namespace std {
+typedef unsigned long int size_t;
+}
+
+#endif
