c3ef4068356566ceae3999838cd29e2f6d166913
Yanis002 <35189056+Yanis002@users.noreply.github.com>
2025-09-27 15:34:18 +0200

    match util

---
+++ include/revolution/hbm/math.hpp
@@ -0,0 +1,8 @@
+#ifndef _REVOLUTION_HBM_NW4HBM_MATH_HPP
+#define _REVOLUTION_HBM_NW4HBM_MATH_HPP
+
+#include "revolution/hbm/nw4hbm/math/math_arithmetic.hpp"
+#include "revolution/hbm/nw4hbm/math/math_triangular.hpp"
+#include "revolution/hbm/nw4hbm/math/math_types.hpp"
+
+#endif
